9d9f585bcd6ffbf4ccde75647935ed802609b3c8
Ross Markello <rossmarkello@gmail.com>
2019-03-05 10:05:19 -0500

    [REF] Remove pypmi.datasets submodule

--- pypmi/__init__.py
+++ pypmi/__init__.py
@@ -3,6 +3,12 @@
 __all__ = [
     '__author__', '__description__', '__email__', '__license__',
     '__maintainer__', '__packagename__', '__url__', '__version__',
+    'available_behavior', 'available_biospecimen',
+    'available_datscan', 'available_demographics',
+    'load_behavior', 'load_biospecimen',
+    'load_datscan', 'load_demographics',
+    'fetchable_studydata', 'fetchable_genetics',
+    'fetch_studydata', 'fetch_genetics'
 ]
 
 from ._version import get_versions
@@ -18,3 +24,10 @@ from .info import (
     __packagename__,
     __url__,
 )
+
+from .fetchers import (fetchable_studydata, fetch_studydata,
+                       fetchable_genetics, fetch_genetics)
+from .loaders import (available_biospecimen, available_behavior,
+                      available_datscan, available_demographics,
+                      load_behavior, load_biospecimen,
+                      load_datscan, load_demographics)
